443a42ca76e6e9528c7e5bdfbb4dda1e9719901e
yangyang01 <yangyang01@iyunxiao.com>
2019-01-15 15:44:42 +0800

    新建仓库及代码

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var qs = require('querystring');
+var url = require('url');
+
+app.all('*',function (req, res) {
+    var uri = url.format({
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: req.path,
+        search: qs.stringify(req.query)
+    });
+    return req.pipe(request(uri)).pipe(res);
+});
+
+app.listen(8866);
