42d7a08fd8772ef2e034737627f38210c08e757c
yangyang01 <yangyang01@iyunxiao.com>
2019-02-15 11:50:21 +0800

    ip

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var request = require('request');
 var qs = require('querystring');
 var url = require('url');
 app.all('*',function (req, res) {
-    // var uri = url.format({
-    //     protocol: 'https',
-    //     hostname: 'www.google.com',
-    //     pathname: req.path,
-    //     search: qs.stringify(req.query)
-    // });
-    // return req.pipe(request(uri)).pipe(res);
-    res.send("hello world");
+    var uri = url.format({
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: req.path,
+        search: qs.stringify(req.query)
+    });
+    return req.pipe(request(uri)).pipe(res);
 });
-app.listen(80,"0.0.0.0");
+app.listen(80,'0.0.0.0');
